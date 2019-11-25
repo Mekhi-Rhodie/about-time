e7564361468e5400e9aa26f3a75ba731e94ef50c
Mekhi-Rhodie <mekhirhodie@gmail.com>
2019-11-25 14:42:25 -0500

    Add jQuery click event that gives modals a block display with a "slideDown" animation.

---
+++ public/scripts/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#log-in").on("click", function(event){
+        $("#login-modal").slideDown(580).css("display","block")
+    });
+    $("#sign-up").on("click", function(event){
+        $("#signup-modal").slideDown(580).css("display","block")
+    });
+}); 
