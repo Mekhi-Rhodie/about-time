5ddca0f1ea1e5a23f929635057859120162950e0
Mekhi Rhodie <mekhirhodid@gmail.com>
2019-12-12 10:28:28 -0500

    Remove time conversion click event.

--- public/scripts/about-time.js
+++ public/scripts/about-time.js
@@ -7,10 +7,6 @@ $(document).ready(function () {
         const sTime = moment(startTime, "H:mma").format("H:mma");
         const hourDifference = moment.utc(moment(eTime, "H:mma").diff(moment(sTime, "H:mma"))).format("H");
         const minDifference = moment.utc(moment(eTime, "H:mma").diff(moment(sTime, "H:mma"))).format("m");
-        console.log(eTime)
-        console.log(sTime)
-        console.log(hourDifference);
-        console.log(minDifference);
         $("#difference").empty().append(hourDifference + " Hours " + minDifference + " Minutes");
     });
     $("#search-button").on("click", function (event) {
@@ -29,46 +25,20 @@ $(document).ready(function () {
             const gmt = response.zones[0].gmtOffset;
             const currentTime = moment().format("h:mm");
             const timeDiff = moment.utc(moment(currentTime, "h:mma").diff(moment(regionTime, "h:mm a"))).format("h");
-            const cityState =  search + ", " + region;
-//<------------------------------------------------------------------------------------------------------------------------------------------------------------------>
-
-            console.log(regionDate);
-            console.log(regionTime);
-            console.log(timeZone);
-            console.log(gmt);
-            console.log(search);
-            console.log(region);
-            console.log(timeDiff);
-            console.log(currentTime);
-            console.log(cityState);
-//<------------------------------------------------------------------------------------------------------------------------------------------------------------------>
-
-            $("#city-state").empty().prepend(cityState).slideDown;
+            const cityState = search + ", " + region;
+            $("#city-state").empty().prepend(cityState);
             $("#region-time").empty().prepend(regionTime);
             $("#date").empty().prepend(regionDate);
             $("#time-diff").empty().prepend(timeDiff);
-            $("#gmt").empty().append("<strong>"+ "GMT: "+"</strong>" + gmt);
+            $("#gmt").empty().append("<strong>" + "GMT: " + "</strong>" + gmt);
             $("#time-zone").empty().prepend("<strong>" + timeZone + "</strong>");
             //Add switch statement to print out full time zone phrase base on abbreveation.
         });
     });
-    $("#event-submit").on("click", function(event){
+    $("#event-submit").on("click", function (event) {
         event.preventDefault();
         const eventDescrip = $("#event-description").val().trim();
         const eventDate = $("#event-date").val().trim();
-        const eventTime =$("#event-time").val().trim();
-        //---------------------------------------------------------------//
-        console.log(eventDescrip);
-        console.log(eventDate);
-        console.log(eventTime);
-    }); 
-    $("#unit-convert").on("click", function(event){
-        event.preventDefault();
-        const x = $("#unit-select1").val();
-        const y = $("#unit-select2").val();
-        const value = $("#unit-value").val().trim();
-        console.log(x)
-        console.log(y)
-        console.log(value)
+        const eventTime = $("#event-time").val().trim();
     });
 });
