b757bb8243ccb37ef7b8ecd34c55f6abb5085c54
Mekhi Rhodie <46280016+Mekhi-Rhodie@users.noreply.github.com>
2020-01-08 17:17:16 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const app = express();
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
